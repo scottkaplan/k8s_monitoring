8972bb77e8f57b5513202d73a9c0aa214b2eb765
Scott Kaplan <scott@kaplans.com>
2024-02-09 09:05:48 -0800

    Fix prometheus operator config.

--- terraform/3-prometheus.tf
+++ terraform/3-prometheus.tf
@@ -116,18 +116,24 @@ resource "aws_instance" "firefly-prometheus" {
       "sudo chmod 400 /home/ec2-user/.aws/credentials",
       "sudo chmod 755 /home/ec2-user/.aws",
       "sudo usermod -aG docker ec2-user",
+      // AWS authentication
       "aws eks update-kubeconfig --region us-west-1 --name demo",
+      "aws ecr get-login-password --region us-west-1 | sudo docker login --username AWS --password-stdin 775956577581.dkr.ecr.us-west-1.amazonaws.com",
+
+      // Build the container for the service and push to ECR
       "git clone https://github.com/kubesphere/prometheus-example-app.git",
       "cd /home/ec2-user/prometheus-example-app; sudo docker build -t prometheus-example-app .",
-      "aws ecr get-login-password --region us-west-1 | sudo docker login --username AWS --password-stdin 775956577581.dkr.ecr.us-west-1.amazonaws.com",
       "sudo docker tag prometheus-example-app:latest 775956577581.dkr.ecr.us-west-1.amazonaws.com/prometheus-example-app:latest",
       "sudo docker push 775956577581.dkr.ecr.us-west-1.amazonaws.com/prometheus-example-app:latest",
-      "cd /home/ec2-user; git clone https://github.com/scottkaplan/k8s_monitoring.git",
-      "/usr/local/bin/kubectl create -f /home/ec2-user/k8s_monitoring/deployment.yaml",
-      "/usr/local/bin/kubectl create -f /home/ec2-user/k8s_monitoring/prometheus.yaml",
+
+      // Install the prometheus operator
       "mkdir /home/ec2-user/prometheus-operator",
       "curl -sL https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/main/bundle.yaml -o /home/ec2-user/prometheus-operator/bundle.yaml",
       "/usr/local/bin/kubectl create -f /home/ec2-user/prometheus-operator/bundle.yaml",
+
+      "cd /home/ec2-user; git clone https://github.com/scottkaplan/k8s_monitoring.git",
+      "/usr/local/bin/kubectl apply -f /home/ec2-user/k8s_monitoring/k8s/deployment.yaml",
+      "/usr/local/bin/kubectl apply -f /home/ec2-user/k8s_monitoring/k8s/prometheus.yaml",
     ]
   }
 }
