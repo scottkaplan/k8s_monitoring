e73106dd2c0cc5593a8e8c0d445a53ff9f35c25d
Scott Kaplan <scott@kaplans.com>
2024-02-09 16:10:23 -0800

    refactor into bash function.
    test non-root docker user.

--- ansible/config_prometheus.sh
+++ ansible/config_prometheus.sh
@@ -4,38 +4,54 @@ exec >/tmp/config_prometheus.out 2>/tmp/config_prometheus.err
 
 set -x
 
-sudo /usr/bin/yum -y install ansible
-mkdir /tmp/ansible
-for f in packages dot_files kubectl docker; do
-    /usr/bin/wget -O /tmp/ansible/${f}.yaml https://raw.githubusercontent.com/scottkaplan/k8s_monitoring/main/ansible/${f}.yaml
-    sudo ansible-playbook /tmp/ansible/${f}.yaml
-done
+function run_playbooks {
+    sudo /usr/bin/yum -y install ansible
+    mkdir /tmp/ansible
+    for f in packages dot_files kubectl docker; do
+        /usr/bin/wget -O /tmp/ansible/${f}.yaml https://raw.githubusercontent.com/scottkaplan/k8s_monitoring/main/ansible/${f}.yaml
+        sudo ansible-playbook /tmp/ansible/${f}.yaml
+    done
+}
 
-mv /tmp/credentials /home/ec2-user/.aws/
-chmod 400 /home/ec2-user/.aws/credentials
+function aws_credentials {
+    mv /tmp/credentials /home/ec2-user/.aws/
+    chmod 400 /home/ec2-user/.aws/credentials
+}
+
+function aws_authentication {
+    aws eks update-kubeconfig --region us-west-1 --name demo
+    aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin 775956577581.dkr.ecr.us-west-1.amazonaws.com
+}
+
+function build_container {
+    git clone https://github.com/kubesphere/prometheus-example-app.git
+    cd /home/ec2-user/prometheus-example-app; docker build -t prometheus-example-app .
+}
+
+function push_container_to_ecr {
+    docker tag prometheus-example-app:latest 775956577581.dkr.ecr.us-west-1.amazonaws.com/prometheus-example-app:latest
+    docker push 775956577581.dkr.ecr.us-west-1.amazonaws.com/prometheus-example-app:latest
+}
+
+function deploy_ecr_to_k8s {
+    cd /home/ec2-user; git clone https://github.com/scottkaplan/k8s_monitoring.git
+    /usr/local/bin/kubectl apply -f /home/ec2-user/k8s_monitoring/k8s/deployment.yaml
+}
+
+function install_prometheus_operator {
+    mkdir /home/ec2-user/prometheus-operator
+    curl -sL https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/main/bundle.yaml -o /home/ec2-user/prometheus-operator/bundle.yaml
+    /usr/local/bin/kubectl apply --server-side --force-conflicts -f /home/ec2-user/prometheus-operator/bundle.yaml
+    /usr/local/bin/kubectl apply -f /home/ec2-user/k8s_monitoring/k8s/prometheus.yaml
+}
 
 sudo usermod -aG docker ec2-user
 
-# AWS authentication
-aws eks update-kubeconfig --region us-west-1 --name demo
-aws ecr get-login-password --region us-west-1 | sudo docker login --username AWS --password-stdin 775956577581.dkr.ecr.us-west-1.amazonaws.com
-
-# Build the container for the service
-git clone https://github.com/kubesphere/prometheus-example-app.git
-cd /home/ec2-user/prometheus-example-app; sudo docker build -t prometheus-example-app .
-
-# push to ECR
-sudo docker tag prometheus-example-app:latest 775956577581.dkr.ecr.us-west-1.amazonaws.com/prometheus-example-app:latest
-sudo docker push 775956577581.dkr.ecr.us-west-1.amazonaws.com/prometheus-example-app:latest
-
-# Deploy container from ECR to k8s
-cd /home/ec2-user; git clone https://github.com/scottkaplan/k8s_monitoring.git
-/usr/local/bin/kubectl apply -f /home/ec2-user/k8s_monitoring/k8s/deployment.yaml
-
-# Install the prometheus operator
-mkdir /home/ec2-user/prometheus-operator
-curl -sL https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/main/bundle.yaml -o /home/ec2-user/prometheus-operator/bundle.yaml
-/usr/local/bin/kubectl apply --server-side --force-conflicts -f /home/ec2-user/prometheus-operator/bundle.yaml
-/usr/local/bin/kubectl apply -f /home/ec2-user/k8s_monitoring/k8s/prometheus.yaml
+aws_authentication
+run_playbooks
+build_container
+push_container_to_ecr
+deploy_ecr_to_k8s
+install_prometheus_operator
 
 exit 0
