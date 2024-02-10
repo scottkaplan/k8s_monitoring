86731e3b68b7ce4e9f900b5b39041da8ba64d0a3
Scott Kaplan <scott@kaplans.com>
2024-02-09 16:33:40 -0800

    More refactoring.

--- ansible/config_prometheus.sh
+++ ansible/config_prometheus.sh
@@ -19,36 +19,43 @@ function docker_non_root {
 }
 
 function aws_credentials {
-    mv /tmp/credentials /home/ec2-user/.aws/
-    chmod 400 /home/ec2-user/.aws/credentials
+    mv /tmp/credentials $base_dir/.aws/
+    chmod 400 $base_dir/.aws/credentials
 }
 
 function aws_authentication {
     aws eks update-kubeconfig --region us-west-1 --name demo
-    aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin 775956577581.dkr.ecr.us-west-1.amazonaws.com
+    aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin $ecr_server
 }
 
 function build_container {
     git clone https://github.com/kubesphere/prometheus-example-app.git
-    cd /home/ec2-user/prometheus-example-app; docker build -t prometheus-example-app .
+    cd $base_dir/prometheus-example-app; docker build -t prometheus-example-app .
 }
 
 function push_container_to_ecr {
-    docker tag prometheus-example-app:latest 775956577581.dkr.ecr.us-west-1.amazonaws.com/prometheus-example-app:latest
-    docker push 775956577581.dkr.ecr.us-west-1.amazonaws.com/prometheus-example-app:latest
+    docker tag $container_name $ecr_server/$container_name
+    docker push $ecr_server/$container_name
 }
 
 function deploy_ecr_to_k8s {
-    cd /home/ec2-user; git clone https://github.com/scottkaplan/k8s_monitoring.git
-    /usr/local/bin/kubectl apply -f /home/ec2-user/k8s_monitoring/k8s/deployment.yaml
+    cd $base_dir; git clone https://github.com/scottkaplan/k8s_monitoring.git
+    $kubectl apply -f $base_dir/k8s_monitoring/k8s/deployment.yaml
 }
 
 function install_prometheus_operator {
-    mkdir /home/ec2-user/prometheus-operator
-    curl -sL https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/main/bundle.yaml -o /home/ec2-user/prometheus-operator/bundle.yaml
-    /usr/local/bin/kubectl apply --server-side --force-conflicts -f /home/ec2-user/prometheus-operator/bundle.yaml
-    /usr/local/bin/kubectl apply -f /home/ec2-user/k8s_monitoring/k8s/prometheus.yaml
+    mkdir $base_dir/prometheus-operator
+    curl -sL $prometheus_operator_yaml -o $base_dir/prometheus-operator/bundle.yaml
+    # prometheus-operator yaml is too big to process on the client
+    $kubectl apply --server-side --force-conflicts -f $base_dir/prometheus-operator/bundle.yaml
+    $kubectl apply -f $base_dir/k8s_monitoring/k8s/prometheus.yaml
 }
+
+ecr_server=775956577581.dkr.ecr.us-west-1.amazonaws.com
+container_name=prometheus-example-app:latest
+base_dir=/home/ec2-user
+kubectl=/usr/local/bin/kubectl
+prometheus_operator_yaml=https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/main/bundle.yaml
 
 run_playbooks
 docker_non_root
