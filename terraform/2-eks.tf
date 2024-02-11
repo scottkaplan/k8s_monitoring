055abb3eba1c38682b6ecf0b5f07fb0b33cb13ad
Scott Kaplan <scott@kaplans.com>
2024-02-10 18:15:18 -0800

    Beef up k8s nodes.

--- terraform/2-eks.tf
+++ terraform/2-eks.tf
@@ -86,10 +86,10 @@ resource "aws_eks_node_group" "private-nodes" {
   ]
 
   capacity_type  = "ON_DEMAND"
-  instance_types = ["t3.small"]
+  instance_types = ["t3.medium"]
 
   scaling_config {
-    desired_size = 1
+    desired_size = 2
     max_size     = 5
     min_size     = 0
   }
